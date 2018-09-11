fd9f005f271e7972af8327f1fa3a429a6e254e74
Obi Onuora <obimarcus11@gmail.COM>
2018-09-11 09:54:37 -0400

    deleted component2

--- src/App/index.js
+++ src/App/index.js
@@ -1,43 +1,32 @@
 import React, { Component } from "react";
 import "./style.css";
 import FirstPlayer from '../FirstPlayer';
-import SecondPlayer from '../SecondPlayer';
-
 
 class App extends Component {
-constructor(props){
-  super(props)
-
-
+  constructor(props) {
+    super(props)
+  }
   
- 
-}
-liftData =(data)=>{
-  this.setState({data:data});
-}
-squashTheDebate = ()=>{
-console.log('this.squashTheDebate')
+  squashTheDebate = () => {
+    console.log('this.squashTheDebate')
   }
 
-
   render() {
-    return ( 
-    <div className='appContainer'>
-    <div className='overlay'>
-    <div className='playerContainer'>
-    <div>
-    <FirstPlayer liftData = {this.liftData} />
-    </div>
-    <button onClick= {()=>this.squashTheDebate()} className= 'squashButton'>Squash The Debate</button>
-    <div>
-    <SecondPlayer />
-   </div>
-   </div>
-</div>
- 
-    </div>
-    
-  )
+    return (
+      <div className='appContainer'>
+        <div className='overlay'>
+          <div className='playerContainer'>
+            <div>
+              <FirstPlayer  title='Home Alone' date='1990-11-09' pic='/5Lo3sWuvbO4AnrAHYBgB5U1Opqd.jpg'/>
+            </div>
+            <button onClick={() => this.squashTheDebate()} className='squashButton'>Squash The Debate</button>
+            <div>
+              <FirstPlayer title='The Notebook' date='2004-06-25' pic='/gMfstesBXKdsHToAUXVPHujUDfb.jpg' />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/FirstPlayer/index.js
+++ src/FirstPlayer/index.js
@@ -5,18 +5,13 @@ export default class FirstPlayer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
-      movieTitle: 'Home Alone',
-      releaseDate: '1990-11-09',
-      moviePic: '/5Lo3sWuvbO4AnrAHYBgB5U1Opqd.jpg',
+      movieTitle: this.props.title,
+      releaseDate: this.props.date,
+      moviePic: this.props.pic,
       obiMeter: '',
       searchMovie: '',
     }
   }
-
-
-
-
 
   fetchMovieData = async () => {
     const API_KEY = '13f56fa7645fad328dd8b8c35c7a3f48';
@@ -24,85 +19,47 @@ export default class FirstPlayer extends Component {
     const apiData = await fetch(apiURL);
     const jsonApi = await apiData.json();
     console.log('this is jsonApi', jsonApi)
-    // console.log('this is jsonApi.results[0]', this.state.moviePic)
     this.setState({
       movieTitle: jsonApi.results[0].original_title,
       releaseDate: jsonApi.results[0].release_date,
       moviePic: jsonApi.results[0].poster_path,
       obiMeter: jsonApi.results[0].vote_average,
-    }, ()=> this.move())
-    // console.log('this is jsonApi.results[0]', this.state.obiMeter)
+    })
   }
-
-
 
   handleChange = (event) => {
     this.setState({ searchMovie: event.target.value });
   }
 
   handleSubmit = (event) => {
-
-    console.log('Person 1 has selected this movie ' + encodeURIComponent(this.state.searchMovie));
     event.preventDefault();
     this.fetchMovieData();
-    // console.log('pic you need ' + this.moviePic)
-   
   }
 
-mainButton = () =>{
-  this.props.liftData()
-  
-}
-
-
-
-   move = ()=> {
-    var elem = document.querySelector(".insideMeter");   
-    var width = 1;
-    var id = setInterval(() => {
-      if (width >= this.state.obiMeter*10) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-      }
-      
-    }, 100);
-    console.log(this.state.obiMeter)
-
+  mainButton = () => {
+    this.props.liftData()
   }
-  
-
 
   render() {
+    const style = {
+      width: (this.state.obiMeter * 10).toString() +'%',
+      transition: 'all 3s',
+    }
+
     return (
       <div className='container'>
-
-      
-
         <div className='movieContainer'>
-
-         <div className='containerMeter'>
-      <div className='insideMeter'></div>
-      </div>
-
+          <div className='containerMeter'>
+            <div className='insideMeter' style={style}></div>
+          </div>
           <img className='selectedMovie' src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.state.moviePic}`} />
           <form className='form form1' onSubmit={this.handleSubmit} >
             <input className='searchBar searchBar1' placeholder='Search 1st Movie' type="text" value={this.state.value} onChange={this.handleChange} />
-            <input className='button searchButton searchButton1' type="submit" value="Submit"  />
+            <input className='button searchButton searchButton1' type="submit" value="Submit" />
             <p>{this.state.movieTitle}<br></br>{this.state.releaseDate}</p>
           </form>
-          
-     
-
         </div>
-
-
- 
-
-
       </div>
-
     )
   }
 }
